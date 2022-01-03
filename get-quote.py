7d82ee752f606e1bb12169af6ab40e77609aa366
UniqueAjith <90856682+UniqueAjith@users.noreply.github.com>
2022-01-03 11:43:50 +0530

    Print two Quotes get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,13 @@
-import random
+import random 
 
 def primary(): 
-    last = 13
-    rnd = random.randint(0, last)
-    f = open("quotes.txt")
-    quotes = f.readlines()
-    f.close()
-    print(quotes[rnd])
+    f = open("quotes.txt") 
+    quotes = f.readlines() 
+    f.close() 
+    #last=18
+    res = random.sample(quotes,2)
+    for i in res:
+        print(i,end="")
 
-if __name__ == "__main__":
+if __name__== "__main__": 
     primary()
